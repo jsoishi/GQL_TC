a39dbe23bbabe4e23a8519b285f4d9c5cbfb89fc
J. S. Oishi <jsoishi@gmail.com>
2022-08-15 08:42:07 -0700

    updated KE vs time plots.

--- python/plot_KE_DNS_GQL.py
+++ python/plot_KE_DNS_GQL.py
@@ -3,13 +3,12 @@ import h5py
 from series import RunSeries
 import matplotlib.pyplot as plt
 import numpy as np
-#from paths import rei640_reo_1359
-from paths import rei900_reo_3398
+import paths
 
 plt.style.use('prl')
-#path = RunSeries(rei640_reo_1359, 'rei640_reo_-1359')
-path = RunSeries(rei900_reo_3398, 'rei900_reo_-3398')
-
+#path = RunSeries(paths.rei640_reo_1359, 'rei640_reo_-1359')
+#path = RunSeries(paths.rei900_reo_3398, 'rei900_reo_-3398')
+path = RunSeries(paths.rei700_reo_3398, 'rei700_reo_-3398')
 filetype = Path("scalar/scalar.h5")
 path.data = []
 path.t = []
@@ -23,12 +22,15 @@ for i, r in enumerate(path.run_list):
     label = path.Lambdaz[i]
 
     if label is None:
-        label = '\infty'
+        label = 'DNS'
+    else:
+        label = f"$\Lambda = {label}$"
     print(f"Lambda = {label} number of samples = {path.data[i].shape[0]}")
-    plt.plot(path.t[i]/t_visc, path.data[i], label=f"$\Lambda = {label}$")
+    plt.plot(path.t[i]/t_visc, path.data[i], label=label)
 plt.xlabel(r"$t/\tau_\nu$")
 plt.ylabel(r"$E_{kin}$")
-plt.xlim(1,2.5)
-plt.legend()
+
+plt.xlim(3,4.5)
+plt.legend(ncol=2,fontsize=16)
 plt.tight_layout()
 plt.savefig(f"../figs/{path.name}_KE_vs_t.pdf")
